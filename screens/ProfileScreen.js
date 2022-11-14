0925bd36bba9ca75d13f70e339c0605c3957f305
Karim El Jazzar <jazzar.karim@gmail.com>
2022-11-14 14:13:55 -0500

    fixed profile screen and added sign out button

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { auth } from '../firebase';
 import { db } from '../firebase'
-import { collection, doc, setDoc, addDoc, updateDoc, deleteDoc, getDoc, getDocs, where, query } from "firebase/firestore/lite"; 
+import { collection, doc, setDoc, addDoc, updateDoc, deleteDoc, getDoc, getDocs, where, query } from "firebase/firestore/lite";
+import { useNavigation } from '@react-navigation/core'
   
 const ProfileScreen = () => {
+
+  const navigation = useNavigation()
 
   const userLoggedInID = auth.currentUser?.uid
 
@@ -22,6 +25,14 @@ const ProfileScreen = () => {
 
   const [userLogged,setUserLogged] = useState({});
 
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
 
   useEffect(() => {
     getUserDetails(userLoggedInID).then(userDetails => {
@@ -30,14 +41,44 @@ const ProfileScreen = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Ionicons name="md-person-circle-outline" size={80} color="#000000" />
-      <Text style={{ color: "#000000", fontSize: 20 }}>{"Name: " + userLogged.firstName + " " + userLogged.lastName}</Text>
-      <Text style={{ color: "#000000", fontSize: 12 }}>{"Email: " + userLogged.email}</Text>
-      <Text style={{ color: "#000000", fontSize: 12 }}>{"Address: " + userLogged.address}</Text>
+    <View style={styles.container}>
+      <Ionicons name="md-person-circle-outline" size={80} color="white" />
+      <Text style={{ color: "white", fontSize: 20 }}>{"Name: " + userLogged.firstName + " " + userLogged.lastName}</Text>
+      <Text style={{ color: "white", fontSize: 12 }}>{"Email: " + userLogged.email}</Text>
+      <Text style={{ color: "white", fontSize: 12 }}>{"Address: " + userLogged.address}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.item}
+      >
+        <Text style={styles.backTxt}>Sign out</Text>
+      </TouchableOpacity>
       
     </View>
   );
 };
   
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    paddingTop: 35,
+    paddingHorizontal: 15
+  },
+  backTxt: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  item: {
+      backgroundColor: '#095A47',
+      padding: 10,
+      marginVertical: 15,
+      borderRadius: 5,
+      width:'50%',
+    },
+});
+
 export default ProfileScreen;
