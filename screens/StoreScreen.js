bedaf9ff89d4cfcd09104d0a0fe2a722f9572767
Daniel Miolan <miolan96@gmail.com>
2022-11-14 08:19:07 -0500

    Get and use product data from DB.

--- screens/StoreScreen.js
+++ screens/StoreScreen.js
@@ -1,28 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { useNavigation } from '@react-navigation/core'
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View, TextInput, ScrollView, Pressable, FlatList, Image } from 'react-native'
-import { auth } from '../firebase'
+import { Dimensions, StyleSheet, Text, View, TextInput, Pressable, FlatList, Image } from 'react-native'
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from '@expo/vector-icons'; 
 import SafeAreaView from 'react-native-safe-area-view';
 import { db } from '../firebase'
-import { collection, doc, setDoc, addDoc, updateDoc, deleteDoc, getDoc, getDocs, where, query } from "firebase/firestore/lite"; 
-
-import img1 from '../assets/ipad.jpeg'
-import img2 from '../assets/lamp.jpeg'
-import img3 from '../assets/mirror.webp'
-import img4 from '../assets/monitor.webp'
-import img5 from '../assets/pc.jpeg'
-import img6 from '../assets/speaker.jpeg'
-import img7 from '../assets/toaster.jpeg'
-import img8 from '../assets/tv.jpeg'
+import { collection, getDocs, where, query, startAt, limit } from "firebase/firestore/lite"; 
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { async } from "@firebase/util";
 
 const StoreScreen = ({navigation}) => {
     const [categories, setCatgories] = useState([]);
+    const [products, setProducts] = useState([]);
+    const [imgageList, setImageList] = useState([]);
 
     const getAllCategories = async () => {
         try {
-            console.log('TRY');
             const response = await getDocs(collection(db, "category"));
             let tempDoc = [];
       
@@ -31,63 +23,105 @@ const StoreScreen = ({navigation}) => {
             });
       
             setCatgories(tempDoc);
-          } catch(err) { console.log(err);}
+          } catch(err) { }
+    }
+
+    const getAllProducts = async () => {
+        try {
+            const q = query(collection(db, "product"), limit(10));
+
+            const response = await getDocs(q);
+            let tempDoc = [];
+
+            response.forEach((doc) => {
+                const data = doc.data();
+        
+                tempDoc.push({
+                    id: doc.id,
+                    img: '',
+                    category: data.category,
+                    description: data.description,
+                    discount: data.discount,
+                    discount_end: data.discount_end,
+                    discount_start: data.discount_start,
+                    name: data.name,
+                    price: data.price,
+                    quantity: data.quantity,
+                    url: data.url,
+                    userID: data.userID
+                });
+            });
+
+            //setProducts(tempDoc);
+            
+            console.log('======')
+            console.log(tempDoc);
+            console.log('======')
+            getImageFromUrl(0, tempDoc)
+          } catch(err) { }
+    }
+
+    const getImageFromUrl = async (index, list) => {
+        let tempIdx = index;
+        let tempList = list;
+        console.log('============')
+        console.log('============')
+        console.log('INDEX = ', index, ' URL = ', tempList[tempIdx].url)
+        const storage = getStorage();
+        const gsReference = ref(storage, products[tempIdx].url);
+        await getDownloadURL(gsReference).then((url) => {
+            console.log(url);
+            tempList[tempIdx].img = url;
+            tempIdx++;
+            if(tempIdx < tempList.length) {
+                //tempList.push(url);
+                getImageFromUrl(tempIdx, tempList);
+            } else {
+                //setImageList(tempList);
+                setProducts(tempList);
+                console.log('=========');
+                console.log(products);
+            }
+        })
+        .catch((error) => { console.log(error) });
     }
 
     useEffect(() => {
         getAllCategories();
+        getAllProducts();
     }, []);
 
-  const dummyDate = [
-    { name: 'iPad', desc: '', img: img1, price: 599.99, discount: 0, qty: 2 },
-    { name: 'Lamp', desc: '', img: img2, price: 25.00, discount: 15, qty: 2 },
-    { name: 'Mirror', desc: '', img: img3, price: 48.15, discount: 0, qty: 2 },
-    { name: 'Monitor', desc: '', img: img4, price: 139.99, discount: 0, qty: 2 },
-    { name: 'PC', desc: '', img: img5, price: 1678.0, discount: 0, qty: 2 },
-    { name: 'Speaker', desc: '', img: img6, price: 94.00, discount: 20, qty: 1 },
-    { name: 'Toaster', desc: '', img: img7, price: 35.15, discount: 0, qty: 2 },
-    { name: 'TV', desc: '', img: img8, price: 688.14, discount: 0, qty: 2 },
-    { name: 'iPad', desc: '', img: img1, price: 599.99, discount: 0, qty: 2 },
-    { name: 'Lamp', desc: '', img: img2, price: 25.00, discount: 15, qty: 2 },
-    { name: 'Mirror', desc: '', img: img3, price: 48.15, discount: 0, qty: 2 },
-    { name: 'Monitor', desc: '', img: img4, price: 139.99, discount: 0, qty: 2 },
-    { name: 'PC', desc: '', img: img5, price: 1678.0, discount: 0, qty: 2 },
-    { name: 'Speaker', desc: '', img: img6, price: 94.00, discount: 20, qty: 2 },
-    { name: 'Toaster', desc: '', img: img7, price: 35.15, discount: 0, qty: 2 },
-    { name: 'TV', desc: '', img: img8, price: 688.14, discount: 0, qty: 2 }
-  ];
+    const [catN, setCatN] = useState(-1);
+    const [keyWord, setKeyWord] = useState('');
+    const [page, setPage] = useState(1);
 
-  const [catN, setCatN] = useState(-1);
-  const [keyWord, setKeyWord] = useState('');
-  const [page, setPage] = useState(1);
+    const selectCategory = (value) => {
+        setCatN(value);
+        // TO - DO
+        // SEARCH BY CATEGORY
+    }
 
-  const selectCategory = (value) => {
-    setCatN(value);
-    // TO - DO
-    // SEARCH BY CATEGORY
-  }
+    const searchByKeyword = () => {
+        // TO - DO
+        // PERFORM SEARCH BY KEYWORD
+        console.log(keyWord);
+    }
 
-  const searchByKeyword = () => {
-    // TO - DO
-    // PERFORM SEARCH BY KEYWORD
-    console.log(keyWord);
-  }
+    const nextPage = () => {
+        let tempPage = page + 1;
+        tempPage = tempPage > 2 ? 2 : tempPage;
+    }
 
-  const nextPage = () => {
-    let tempPage = page + 1;
-    tempPage = tempPage > 2 ? 2 : tempPage;
-  }
+    const prevPage = () => {
+        let tempPage = page - 1
+        tempPage = tempPage < 1 ? 1 : tempPage;
+    }
 
-  const prevPage = () => {
-    let tempPage = page - 1
-    tempPage = tempPage < 1 ? 1 : tempPage;
-  }
-
-  const openProduct = (index) => {
-    // TO - DO
-    // PUSH DETAIL PAGE ON NAVIGATION WITH PRODUCT DATA
-    navigation.navigate('Product', { product: dummyDate[index] })
-  }
+    const openProduct = (index) => {
+        // TO - DO
+        // PUSH DETAIL PAGE ON NAVIGATION WITH PRODUCT DATA
+        navigation.navigate('Product', { product: dummyDate[index] })
+    }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -120,11 +154,11 @@ const StoreScreen = ({navigation}) => {
           }/>
       </View>
       
-        <FlatList style={styles.scroll} nestedScrollEnabled numColumns={2} data={dummyDate} renderItem={({ item, index }) => 
+        <FlatList style={styles.scroll} nestedScrollEnabled numColumns={2} data={products} renderItem={({ item, index }) => 
           <Pressable style={[styles.card, index % 2 == 0 ? styles.colOne : styles.colTwo ]} onPress={() => openProduct(index)}>
             <View style={styles.cardBG}>
               <View style={styles.cardImgCover}>
-                <Image style={styles.cardImg} source={item.img}/>
+                <Image style={styles.cardImg} source={{uri: item.img}}/>
                 { item.discount > 0 ? 
                 <>
                   <Text style={styles.cardDeal}>{item.discount}% off</Text>
@@ -139,7 +173,7 @@ const StoreScreen = ({navigation}) => {
                   <>
                     <Text style={[styles.cardPrice, styles.cardPriceLine, {color: '#6B6B6B'}]}>${item.price}</Text>
                     <Text style={[styles.cardPrice, {color: '#6B6B6B'}]}> - </Text>
-                    <Text style={styles.cardPrice}>${item.price * ((100 - item.discount) / 100)}</Text>
+                    <Text style={styles.cardPrice}>${(item.price * ((100 - item.discount) / 100)).toFixed(2)}</Text>
                   </>
                 :
                   <Text style={styles.cardPrice}>${item.price}</Text>
